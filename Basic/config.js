53de78a014fe64f9a17ec084d33d7e842e6e5ace
Cristhian Sanchez <crivisan1994@gmail.com>
2025-07-31 17:49:00 +0200

    feat: added control 3d controls and more projections

--- Basic/config.js
+++ Basic/config.js
@@ -4,12 +4,18 @@ const Config = {
         fetchBroadcastUrl: "./resources/newsFeedPortalAlerts.json"
     },
     namedProjections: [
-        ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
-    ],
+        // GK DHDN
+        ["EPSG:31467", "+title=Bessel/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+        // ETRS89 UTM
+        ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"],
+        // LS 320: zusammen mit Detlef Koch eingepflegt und geprüft
+        ["EPSG:8395", "+title=ETRS89/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=GRS80 +datum=GRS80 +units=m +no_defs"],
+        // WGS84
+        ["EPSG:4326", "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"]],
     layerConf: "./resources/services-internet.json",
     restConf: "./resources/rest-services-internet.json",
     styleConf: "./resources/style_v3.json",
-    wfsImgPath: "./resources/img/",
+    wfsImgPath: "https://geodienste.hamburg.de/lgv-config/img/",
     portalLanguage: {
         enabled: true,
         debug: false,
